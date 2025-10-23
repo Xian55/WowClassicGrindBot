c73df9a64e9e7ad005696fe418f9dc9675c0cfa8
Xian55 <367101+Xian55@users.noreply.github.com>
2025-10-23 16:41:06 +0200

    SoundKitBackport uses a UI safe implementation

--- Addons/SoundKitBackport/SoundKitBackport.lua
+++ Addons/SoundKitBackport/SoundKitBackport.lua
@@ -1,16 +1,17 @@
 -- Backport SOUNDKIT for pre-WoD clients (Cataclysm 4.3.4)
 if SOUNDKIT then return end
 
-setmetatable(_G, {
-    __index = function(_, key)
-        if key == "SOUNDKIT" then
-            local t = setmetatable({}, {
-                __index = function(_, soundName)
-                    return string.lower(soundName)
-                end
-            })
-            rawset(_G, "SOUNDKIT", t)
-            return t
-        end
-    end
-})
+-- Cataclysm-safe SOUNDKIT backport
+SOUNDKIT = {
+    IG_ABILITY_OPEN           = "igAbilityOpen",
+    IG_ABILITY_CLOSE          = "igAbilityClose",
+    IG_MAINMENU_OPEN          = "igMainMenuOpen",
+    IG_MAINMENU_CLOSE         = "igMainMenuClose",
+    IG_CHARACTER_INFO_OPEN    = "igCharacterInfoOpen",
+    IG_CHARACTER_INFO_CLOSE   = "igCharacterInfoClose",
+    IG_SPELLBOOK_OPEN         = "igSpellBookOpen",
+    IG_SPELLBOOK_CLOSE        = "igSpellBookClose",
+    IG_BACKPACK_OPEN          = "igBackPackOpen",
+    IG_BACKPACK_CLOSE         = "igBackPackClose",
+    -- add any other ones BindPad uses here
+}
